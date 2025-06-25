25cb9c811d5206f81aa09718cea1daf92c5d8f01
PureshkaM <khripunov_26@mail.ru>
2025-06-25 13:29:44 +0500

    task2,v1.0

--- tasks/tasks/tasks.cs
+++ tasks/tasks/tasks.cs
@@ -6,14 +6,47 @@
 
 Далее вернуть пользователю обработанную строку.*/
 
+
+
+/*Добавить в программу из «Задания 1» дополнительный функционал. Необходимо начать проверять входящую строку, чтобы в ней были только буквы из
+ * английского алфавита в нижнем регистре «abcdefghijklmnopqrstuvwxyz». Результатом работы программы должна стать либо обработанная строка, как в «Задании 1» 
+ * или сообщение о том, что были введены не подходящие символы, в сообщении об ошибке должны быть перечислены все неподходящие символы, которые были введены
+
+Результат программы:
+
+Если не подходящая строка:
+
+Сообщение об ошибке с информацией
+
+Если подходящая строка:
+
+Обработанная строка*/
+
+
+
 public class StringProcessor
 {
     public static void Main(string[] args)
     {
         Console.Write("Введите строку: ");
         string input = Console.ReadLine();
-        string result = Process(input);
-        Console.WriteLine("Результат: " + result);
+
+        var invalidChars = GetInvalidCharacters(input);
+
+        if (invalidChars.Any())
+        {
+            Console.WriteLine("Ошибка: введены недопустимые символы: " + string.Join(", ", invalidChars));
+        }
+        else
+        {
+            string result = Process(input);
+            Console.WriteLine("Результат: " + result);
+        }
+    }
+
+    private static List<char> GetInvalidCharacters(string input)
+    {
+        return input.Where(c => c < 'a' || c > 'z').Distinct().ToList();
     }
 
     private static string Process(string input)
